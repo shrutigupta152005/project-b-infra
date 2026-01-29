3f40531d9a6bf75a10cb68da48620e1b3cda71c9
Shruti Gupta <shrutigupta152005@gmail.com>
2026-01-29 17:27:13 +0530

    Change EC2 instance type from t2.micro to t3.micro

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,7 +1,7 @@
 module "ec2" {
   source        = "git::https://github.com/shrutigupta152005/terraform-modules.git//ec2"
   ami           = "ami-0061e2cfb716149cb"   # Same Frankfurt AMI
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   name          = "project-b-server"
 }
 #project b infra
