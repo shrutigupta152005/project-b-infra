97f042b71cafeabffa8d98711a1c2283b1cd15c1
Shruti Gupta <shrutigupta152005.com>
2026-01-29 15:49:19 +0530

    Initial commit: Project-B infra + workflow

---
+++ envs/dev/main.tf
@@ -0,0 +1,6 @@
+module "ec2" {
+  source        = "git::https://github.com/shrutigupta152005/terraform-modules.git//ec2"
+  ami           = "ami-0061e2cfb716149cb"   # Same Frankfurt AMI
+  instance_type = "t2.micro"
+  name          = "project-b-server"
+}
